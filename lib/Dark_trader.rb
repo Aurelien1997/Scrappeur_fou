acc94bf6045520c73898bf5718bf26f526a98265
Aurelien1997 <93325069+Aurelien1997@users.noreply.github.com>
2022-01-27 18:25:39 +0000

    Add files via upload

---
+++ lib/Dark_trader.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+
+def scraper
+    url = "https://coinmarketcap.com/all/views/all/"
+    unparsed_page = URI.open(url).read
+    parsed_page = Nokogiri::HTML(unparsed_page)
+
+    name = parsed_page.css('a.cmc-table__column-name--name.cmc-link').find_all
+    name_array = name.collect(&:text) 
+
+    price = parsed_page.css('div.cLgOOr').find_all
+    price_array = price.collect(&:text) 
+
+    puts " "
+
+    new_array = name_array.zip(price_array)
+    print new_array
+end
+
+scraper
+
+
